eb90e27fa023d4a99309d0a2b7f4ee5dcad66cb0
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-10-25 19:40:04 -0300

    Hello world básico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  mensagem = 'Nenhum oi';
+  nome: string = 'Maria';
 
   constructor() {}
 
+  clickOk() {
+    if (this.nome) {
+      this.mensagem = `Hello world, ${this.nome}!`;
+    } else {
+      this.mensagem = '';
+    }
+  }
+
+  changeNome(event: any) {
+    console.log(event);
+    this.nome = event.target.value.trim();
+  }
 }
