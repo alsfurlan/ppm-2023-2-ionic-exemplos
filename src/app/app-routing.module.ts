6df250569edf8139de12d5565b6225a5618787f4
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-11-01 21:05:46 -0300

    Adicionado rotas a aplicação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,41 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {
+  PreloadAllModules,
+  RouterModule,
+  Routes,
+} from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { LivroListaComponent } from './livro/components/livro-lista/livro-lista.component';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./home/home.module').then(
+        (m) => m.HomePageModule
+      ),
+  },
+  {
+    path: 'hello-world',
+    component: HelloWorldComponent,
+  },
+  {
+    path: 'livros',
+    component: LivroListaComponent,
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+    }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { HelloWorldComponent } from '../hello-world/hello-world.component';
+import { LivroListaComponent } from '../livro/components/livro-lista/livro-lista.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+  },
 ];
 
 @NgModule({
